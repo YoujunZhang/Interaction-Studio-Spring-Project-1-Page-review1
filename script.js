3af1f3b3c140f6500d6037157404a4ae2224544b
YoujunZhang <144183143+YoujunZhang@users.noreply.github.com>
2024-02-22 19:05:36 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function myFunction(element, clr) {
+    element.style.color = clr;
+}
+
+
+// new cursoreffects.fairyDustCursor({
+//     colors: ["#FFFEB9", "#FFFFE1", "#FFBE0F"],
+//   });
+
+
+
+
+function myFunction() {
+    location.replace("https://youjunzhang.github.io/Interaction-Studio-Spring-Project-1-Page-3/")
+  }
